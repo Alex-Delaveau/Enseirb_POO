cdb66ae324f7b922332ef39d4cbaba12ad7297f1
Alex Delaveau <alex@PC-ALEX>
2024-03-25 14:42:37 +0100

    Ready for scenario

--- porte/heritage/Scenario.java
+++ porte/heritage/Scenario.java
@@ -4,6 +4,7 @@ public class Scenario {
     
 
     public static void main(String[] args) {
-        
+        Porte porteCharniere = new PorteCharniere();
+        Porte porteCoulissante = new PorteCoulissante(10);
     }
 }
---
+++ porte/heritage/Telecommande.java
@@ -0,0 +1,10 @@
+package porte.heritage;
+
+import java.util.ArrayList;
+
+public class Telecommande {
+
+    private final int maxPorte;
+  private final ArrayList<Porte> mesPortes;
+    
+}
