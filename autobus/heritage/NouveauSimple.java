96cf3275a48961798b9c4bce74bc868e65adbe39
Alex Delaveau <alex@DESKTOP-LRUME82>
2024-03-28 09:42:06 +0100

    nouveau simple

---
+++ autobus/heritage/NouveauSimple.java
@@ -0,0 +1,37 @@
+package autobus.heritage;
+
+import autobus.heritage.tec.PassagerAnxieux;
+import autobus.heritage.tec.PassagerStresse;
+import autobus.heritage.tec.Usager;
+
+public class NouveauSimple {
+    public static void main(String[] args) {
+        Autobus serenity = new Autobus(1, 2);
+        Usager zoe = new PassagerAnxieux("Zoe", 3);
+        Usager wash = new PassagerStresse("Wash", 4);
+
+
+        System.out.println(serenity);
+
+
+        serenity.allerArretSuivant();
+        zoe.monterDans(serenity);
+
+
+        System.out.println(serenity);
+        System.out.println(zoe);
+
+
+        serenity.allerArretSuivant();
+        wash.monterDans(serenity);
+
+
+        System.out.println(serenity);
+        System.out.println(zoe);
+        System.out.println(wash);
+
+
+        // Etc…
+    }
+
+}
