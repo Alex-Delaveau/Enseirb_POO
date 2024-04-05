1c76cd2bf6213a695769467b099ffc143148c985
Alex Delaveau <alex@DESKTOP-LRUME82>
2024-04-05 09:00:57 +0200

    Add destination assignment in PassagerStresse constructor

--- autobus/heritage/tec/PassagerLunatique.java
+++ autobus/heritage/tec/PassagerLunatique.java
@@ -2,6 +2,7 @@ package autobus.heritage.tec;
 
 public class PassagerLunatique extends PassagerStandard {
 
+
     public PassagerLunatique(String nom, int destination) {
         super(nom, destination);
     }
